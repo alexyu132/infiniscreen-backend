3a4743a84f3f9d385b17b45bc46ac23f99d015e7
Alex Yu <alexyu@umd.edu>
2018-09-08 17:49:59 -0400

    initial implementation

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express'),
+      io = require('socket.io');
+
+let app = express(), 
+    socket;
+
+let rooms = []; // array of rooms
+
+function initRoom(client) {
+    let roomNumber = 1 + Math.random() * 100000 | 0, unique;
+  
+    do{
+      unique = true;
+      for(let i = 0; i < rooms.length; i++) {
+        if(rooms[i].roomNumber == roomNumber) {
+          unique = false;
+          roomNumber = Math.random() * 10000 | 0;
+          break;
+        }
+      }
+    } while(!unique);
+  
+    rooms.push({
+      roomNumber: roomNumber,
+      hostId: client.id,
+      clients: []
+    });
+  
+    console.log("Creating room with host " + client.id + " and room number " + roomNumber);
+  
+    return roomNumber;
+  }
+
+  function getRoomByHost(host) {
+    for(let i = 0; i < rooms.length; i++) {
+      if(rooms[i].hostId == host) {
+        return rooms[i];
+      }
+    }
+    return null;
+  }
+
+  function getRoomIndexByNumber(number) {
+    for(let i = 0; i < rooms.length; i++) {
+      if(rooms[i].roomNumber == number) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  socket = io(app.listen(process.env.PORT || 8080, function() {
+    console.log("Server running at port " + this.address().port);
+  }));
+
+  socket.on("connection", function(client) {
+    console.log("Connection from id " + client.id);
+
+    client.on("disconnect", function(client) {
+        let roomNumber = getRoomByHost(client.id);
+    
+        // If this user is a host, disconnect all the clients in the room
+        if(roomNumber !== null) {
+            io.sockets.clients(String(roomNumber)).forEach(function(s){
+                s.disconnect(true);
+            });
+
+            let index = getRoomIndexByNumber(roomNumber);
+            rooms.splice(index, 1);
+        }
+       
+    });
+
+    // CLIENT EVENTS
+
+    client.on("client_join", function(roomNumber) {
+        let index = getRoomIndexByNumber(roomNumber);
+        if(index > -1) {
+            rooms[index].clients.push(client.id);
+            client.join(String(roomNumber));
+        }
+    });
+
+    // HOST EVENTS
+
+    client.on("host_start", function() {
+        let roomNumber = initRoom(client);
+        client.join(String(roomNumber));
+        client.emit("code_assignment", roomNumber);
+    });
+
+    client.on("pause", function(timestamp) {
+        let roomNumber = getRoomByHost(client.id);
+        if(roomNumber !== null) {
+            socket.to(String(roomNumber)).emit("pause", {"timestamp": timestamp});
+        }
+    });
+
+    client.on("play", function(timestamp) {
+        let roomNumber = getRoomByHost(client.id);
+        if(roomNumber !== null) {
+            socket.to(String(roomNumber)).emit("play");
+        }
+    });
+
+    client.on("video_url", function(url) {
+        let roomNumber = getRoomByHost(client.id);
+        if(roomNumber !== null) {
+            socket.to(String(roomNumber)).emit("video_url", {"url":url});
+        }
+    });
+  });
+
+ 
+
+ 
+
+  
+  
